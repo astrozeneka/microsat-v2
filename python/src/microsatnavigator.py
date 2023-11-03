c492f0e3269d3567faa50499a9d61b956cbf727a
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2023-11-03 17:02:56 +0700

    Update the microsatnavigator

--- python/src/microsatnavigator.py
+++ python/src/microsatnavigator.py
@@ -7,14 +7,21 @@ from Bio import SeqIO
 from glob import glob
 import os
 
+
+import argparse
+parser = argparse.ArgumentParser(description='MicrosatNavigator: Analyze microsatellite')
+parser.add_argument('--fasta', help='Fisher association data')
+args = parser.parse_args()
+
 def search_microsat(keyword, min_repeats, use_cache):
-    file_name = f"../data/fasta/{keyword[:3]}/{keyword}.fasta"
+    file_name = args.fasta
     print(f"Search microsatellites in {keyword}")
     record = SeqIO.read(file_name, "fasta")
     tsv_file = f"../data/tmp/{keyword}.tsv"
     if use_cache and os.path.isfile(tsv_file):
         return
     res = microsatellite.searchssr(str(record.seq), min_repeats)
+    tsv_file = args.fasta.replace(".fasta", ".tsv").replace(".fna", ".tsv").replace(".fa", ".tsv")
     with open(tsv_file, "w") as f:
         f.write("\n".join(["\t".join([str(a) for a in b]) for b in res]))
     return res
@@ -35,4 +42,5 @@ if __name__ == '__main__':
     chr_list = genome_chromosome_list(GENOME)
 
     #rec = SeqIO.read("../data/LAG01.fasta", "fasta")
-    #search_microsat("LAG1", MIN_REPEATS, USE_CACHE)
+    search_microsat("LAG1", MIN_REPEATS, USE_CACHE)
+    print()
